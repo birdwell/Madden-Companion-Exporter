153d6a5879416ab81e2676b58ac079cae2a089ed
dmvblc <dmvblc@gmail.com>
2023-10-24 12:52:06 -0400

    Update app.js with firebase info

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ const app = express();
 // Enter the path to your service account json file below where it says "REPLACE_WITH_SERVICE_ACCOUNT"
 // If you need more help with this step go here: https://firebase.google.com/docs/admin/setup
 
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("./madden-companion-project-firebase-adminsdk-u16ts-0a223df9a2.json");
 
 // TODO: Uncomment out line 17-21
 // Enter your database url from firebase where it says <DATABASE_NAME> below.
 // Refer to picture for reference. It's the 2nd property.
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount),
+   databaseURL: "https://madden-companion-project-default-rtdb.firebaseio.com"
+ });
 
 app.set('port', (process.env.PORT || 3001));
 
